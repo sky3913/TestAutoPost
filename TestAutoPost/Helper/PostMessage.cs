4b593f8242ccca3e4b86262c0565f00e8a7eb4b9
sky3913 <skylancer3id@yahoo.com>
2014-04-10 00:39:29 +0700

    post

---
+++ TestAutoPost/Helper/PostMessage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+
+namespace TestAutoPost.Helper
+{
+    public class PostMessage
+    {
+        public static void Send(string username, string password, string dlc_content)
+        {
+            byte[] _bytes = Encoding.ASCII.GetBytes(string.Concat("content=", dlc_content));
+
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create("http://kaskus.co.id");
+            string Credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(username + ":" + password));
+
+            httpWebRequest.Accept = "Accept=text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+            httpWebRequest.AllowAutoRedirect = false;
+            httpWebRequest.AllowWriteStreamBuffering = true;
+            httpWebRequest.CookieContainer = new CookieContainer(128);
+            httpWebRequest.ContentType = "application/x-www-form-urlencoded";
+            httpWebRequest.ContentLength = (long)_bytes.Length;
+            httpWebRequest.Headers.Add("Authorization", "Basic " + Credentials);
+            httpWebRequest.Method = "POST";
+            httpWebRequest.Timeout = 10000;
+            httpWebRequest.UserAgent = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; GTB6; SLCC1; .NET CLR 2.0.50727;";
+
+            Stream requestStream = ((WebRequest)httpWebRequest).GetRequestStream();
+            requestStream.Write(_bytes, 0, _bytes.Length);
+            requestStream.Close();
+
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+            if (httpWebResponse == null)
+                return;
+            else
+            {
+                using (StreamReader reader = new StreamReader(httpWebResponse.GetResponseStream()))
+                {
+                    Trace.WriteLine(reader.ReadToEnd());
+                }
+            }
+        }
+    }
+}
---
+++ TestAutoPost/Models/Post.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestAutoPost.Models
+{
+    public class Post
+    {
+        public string MessageTitle { get; set; }
+        public string MessageBody { get; set; }
+    }
+}
